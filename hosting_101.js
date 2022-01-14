35c8bdef8ed61a07c4def7d2efe30b592d701ee3
Tatyana Asenova <77272421+Tatyana-OA@users.noreply.github.com>
2022-01-14 13:38:16 +0200

    Create hosting_101.js
    	Hoisting:
    a.	Variables being hoisted to the top of the scope (JS way of making space for upcoming variables.) A variable that is not yet defined is still hoisted to the top.
    b.	If that hoisted variable is defined with var there is no reference error, an undefined is given
    c.	If a variable is defined with const and let -> it goes into the temporal dead zone, thus is not hoisted and will give a reference error if we try to log it before initialization

---
+++ hosting_101.js
@@ -0,0 +1,9 @@
+console.log(hello);
+console.log(darkness);
+console.log(oldFriend);
+
+
+
+var hello = 'hello'; //gets hoisted
+let darkness = "darkness"; // no hoisting, reference error
+const oldFriend = "old Friend"; // no hoisting, reference error
